96eac5e58d6f3fd9e2d7bb6d93c183f3d2d837e2
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2024-08-04 22:44:11 -0300

    correct migration

--- database/migrations/2024_07_16_164548_create_topic_table.php
+++ database/migrations/2024_07_16_164548_create_topic_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('subject_id')->constrained('subject')->onDelete('cascade');
+            $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade');
             $table->string('name');
             $table->longText('description')->nullable();
             $table->timestamps();
