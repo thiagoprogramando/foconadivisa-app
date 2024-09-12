221710a231c7a4e1a5b18c336bb3642296827dc9
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2024-09-12 02:41:47 -0300

    update migration FAQ

--- database/migrations/2024_09_11_212748_create_faq_table.php
+++ database/migrations/2024_09_11_212748_create_faq_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('faq', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('plan_id')->nullable()->constrained('plans')->onDelete('cascade');
+            $table->foreignId('plan_id')->nullable()->constrained('plan')->onDelete('cascade');
             $table->string('title');
             $table->string('response')->nullable();
             $table->integer('type')->default(1); // 1 is Standard | 2 is Finance | 3 is Product | 4 is Questions | 5 is Awnswer | 6 is notebook
